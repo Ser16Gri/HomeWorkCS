d44a2e83f01ad3941456fae929377a5b7ccbbbfb
Ser16Gri <1gryg1@mail.ru>
2022-12-20 19:51:51 +0300

    Добавили задачу 6

---
+++ HomeWorkCS/Task6/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число ");
+int n = int.Parse(Console.ReadLine());
+
+string s = "";
+
+if (n % 2 ==0)
+{
+    s = "Да";
+}
+else
+{
+    s = "Нет";
+}
+Console.WriteLine(s);
