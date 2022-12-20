6d115b95235df54b4a37ad3a4d99c96d152e247e
Ser16Gri <1gryg1@mail.ru>
2022-12-20 19:17:17 +0300

    Добавили вариант к задаче 8

--- HomeWorkCS/Task8/Program.cs
+++ HomeWorkCS/Task8/Program.cs
@@ -1,8 +1,19 @@
 ﻿Console.WriteLine("Введите число ");
 int n = int.Parse(Console.ReadLine());
 
+//for (int i = 2; i <= n; i++)
+//    if (i % 2 == 0)
+//Console.WriteLine($"Четное число {i}");
 
-for (int i = 2; i <= n; i++)
-    if (i % 2 == 0)
-Console.WriteLine($"Четное число {i}");
+int i = 2;
+
+while(i <= n)
+{
+    if (i % 2 == 0);
+    {
+        Console.WriteLine($"Четное Число {i}");
+    }
+    i = i + 2;
+}
+
     
