ae80456735585609b9b181447581d9eb97427e5c
Ser16Gri <1gryg1@mail.ru>
2022-12-20 13:34:14 +0300

    Написали задачу 2

---
+++ HomeWorkCS/Task2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите певое число: ");
+int numberA = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int numberB = int.Parse(Console.ReadLine());
+
+int max = numberA;
+
+while (numberB > max)
+{
+    max = numberB;
+}
+Console.Write($"Большее число {max}");
