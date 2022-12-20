d5c01b10b2b8cb04abb92431a12e49edd5c986d5
Ser16Gri <1gryg1@mail.ru>
2022-12-20 15:21:21 +0300

    Добавили задачу6

---
+++ HomeWorkCS/Task4/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите певое число: ");
+int numberA = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int numberB = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите третье число: ");
+int numberc = int.Parse(Console.ReadLine());
+
+int max = numberA;
+
+if(numberB > max)
+{
+    max = numberB;
+}
+if(numberc > max)
+{
+    max = numberc;
+}
+Console.Write($"Максимальное число {max}");
