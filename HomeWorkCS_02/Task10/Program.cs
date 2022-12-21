c7a8ba4e610eba507d0bae960cb8915bc8b7ee26
Ser16Gri <1gryg1@mail.ru>
2022-12-21 13:40:01 +0300

    Дбобавили Task10

---
+++ HomeWorkCS_02/Task10/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите трехзначное число");
+int N = int.Parse(Console.ReadLine());
+
+int b = 0;
+
+if (N > 99 && N < 999 )
+{
+    b = (N / 10) % 10;
+    Console.WriteLine($"Вторая чифра этого числа {b}");
+}
+else Console.WriteLine("Вы ввели неправильное число");
+
+
