f3b939dde84437e0bcdf1b8029087bdafef6777e
Ser16Gri <1gryg1@mail.ru>
2022-12-21 16:12:10 +0300

    Добавили Task15

---
+++ HomeWorkCS_02/Task15/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите день недели цифрой");
+int n = int.Parse(Console.ReadLine());
+
+string d = "";
+
+if (n > 0 && n < 6)
+{
+    d = "Нет";
+    Console.WriteLine(d);
+
+}
+else if (n > 5 && n < 8)
+{
+    d = "Да";
+    Console.WriteLine(d);
+}
+else Console.WriteLine("Нет такого дня недели");
