974be968c07e64f7027fb6b208ae0667c156c6f3
Ser16Gri <1gryg1@mail.ru>
2022-12-21 15:20:00 +0300

    Добавили Task13

---
+++ HomeWorkCS_02/Task13/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Введите число");
+int N = int.Parse(Console.ReadLine());
+
+int c = 0;
+
+if (N > 99 && N < 99999)
+{
+    if (N > 99 && N < 999)
+    {
+        c = N % 10;
+        Console.WriteLine($"Третья цифра {c}");
+    }
+        if (N > 9999 && N < 99999)
+        {
+            c = (N / 100) % 10;
+            Console.WriteLine($"Третья цифра {c}");
+        }
+            if (N > 999 && N < 9999)
+            {
+                Console.WriteLine("Вы ввели неправильное число");
+            }
+}
+else Console.WriteLine("Третьей цифры нет");
