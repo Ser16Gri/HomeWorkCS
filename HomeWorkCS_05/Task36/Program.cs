8c5959affd8c4eac7946b66525af0ca7bb9fcfe2
Ser16Gri <1gryg1@mail.ru>
2022-12-27 17:37:22 +0300

    Добавили Task36

---
+++ HomeWorkCS_05/Task36/Program.cs
@@ -0,0 +1,51 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+
+int length = ReadInt("array length");
+int[] array = GetRandomArray(length, -99, 99);
+PrintArray(array);
+Console.WriteLine();
+int sum = GetSumOddPosition(array);
+Console.WriteLine(sum);
+
+
+int ReadInt(string argument)
+{
+    Console.Write($"Input {argument}: ");
+    return int.Parse(Console.ReadLine());
+}
+
+int[] GetRandomArray(int length, int minValue, int maxValue)
+{
+    int[] array = new int[length];
+    Random random = new Random();
+
+    for (int  i = 0; i < array.Length; i++)
+    {
+        array[i] = random.Next(minValue, maxValue + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+}
+
+int GetSumOddPosition (int[] array)
+{
+    int sum = 0;
+    int i = 1;
+    while (i < array.Length)
+    {
+        sum += array[i];
+        i = i + 2;
+    }
+    return sum;
+}
