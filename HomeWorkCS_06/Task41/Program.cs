85228fab925e636387f1c7ae3ba52cd794779b79
Ser16Gri <1gryg1@mail.ru>
2022-12-28 16:31:17 +0300

    Добавили Task41

---
+++ HomeWorkCS_06/Task41/Program.cs
@@ -0,0 +1,30 @@
+﻿// Task41 Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+
+int M = ReadInt("M");
+CountPosNum(M);
+
+
+int ReadInt(string argument)
+{
+    Console.Write($"Input {argument}: ");
+    return int.Parse(Console.ReadLine()!);
+}
+
+void CountPosNum(int M)
+{
+    int count = 0;
+    for (int i = 0; i < M; i++)
+    {
+        int number = ReadInt("number");
+        if (number > 0)
+        {
+            count++;
+        }
+    }
+    Console.WriteLine();
+    Console.WriteLine($"Positive numbers: {count}");
+}
